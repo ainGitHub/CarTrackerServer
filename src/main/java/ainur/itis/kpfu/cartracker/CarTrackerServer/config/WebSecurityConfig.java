279ef4fa50aeaa3ca92ee23137a55a81985fd9f8
ainur <ainur6969@gmail.com>
2018-05-31 23:23:14 +0300

    refactoring

--- src/main/java/ainur/itis/kpfu/cartracker/CarTrackerServer/config/WebSecurityConfig.java
+++ src/main/java/ainur/itis/kpfu/cartracker/CarTrackerServer/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/resources/**").permitAll()
+                    .antMatchers("/resources/**", "/data").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
